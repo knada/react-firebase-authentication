71485379b12b375d2ccd3a86cace0c36ed13a6e4
knada <nanaayimda@gmail.com>
2020-09-30 12:36:46 +0000

    Added logout button and functionality to home page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Firebase authentication with React in Typescript
 
 ## Tech Used
- Redux-Toolkit for state management, React-Router for navigation, Styled-Components for styling, React-Hook-Forms for form handling and Validation in Typescript.
+ Redux with Redux-Toolkit for state management, React-Router for navigation, Styled-Components for styling, React-Hook-Forms for form handling and Validation and everything is written in Typescript.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,44 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
-import { selectAuthUser } from '../../features/Auth/authSlice'
+import { selectAuthUser, logout, setAuthError } from '../../features/Auth/authSlice'
+import { useFirebase } from '../../hooks/contexts'
 import Loading from '../Loading/Loading'
 
 const HomeContainer = styled.div`
     margin: auto;
 `
 
-const HomeHeading = styled.h1``
+const HomeHeading = styled.h1`
+    margin: 2rem;
+    text-align: center;
+`
+
+const LogoutButton = styled.button``
 
 const Home = () => {
     const authUser = useSelector(selectAuthUser)
+    const firebase = useFirebase()
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        firebase
+            .doSignOut()
+            .then(() => {
+                localStorage.removeItem('authenticated')
+                dispatch(logout())
+                history.push('/')
+            })
+            .catch((error: any) => dispatch(setAuthError({ code: error.code, message: error.message })))
+    }
+
     if (authUser) {
         return (
             <HomeContainer>
                 <HomeHeading>Welcome {authUser.email}</HomeHeading>
+                <LogoutButton onClick={handleLogout}>Log out</LogoutButton>
             </HomeContainer>
         )
     }
